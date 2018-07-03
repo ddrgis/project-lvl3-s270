941caefe29bc079378d4a7b44a96aec94d08a90f
ddrgis <kasabiankpp@gmail.com>
2018-07-03 08:50:25 +0200

    Install webpack, configure dev server, jshint. Connect bootstrap

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import half from './half';
+
+console.log('Hi there');
 
 export default half;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'RSS Reader',
+      template: './public/template.html',
+    }),
+  ],
+};
