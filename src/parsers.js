f15684ef78352c52cb24f60756cd70bd3173f9c8
ddrgis <kasabiankpp@gmail.com>
2018-07-05 08:15:08 +0200

    Increase timeout of CORS request to 31 secs.

--- src/parsers.js
+++ src/parsers.js
@@ -1,21 +1,31 @@
 import axios, { get } from 'axios';
 
 const parseRSS = (url) => {
+  get(`https://crossorigin.me/${url}`)
+    .then((response) => {
+      console.log('First:');
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log('First:');
+      console.log(error);
+    });
+
   const instance = axios.create({
     baseURL: 'https://crossorigin.me/',
-    timeout: 20000,
+    timeout: 31000,
     Origin: 'https://crossorigin.me/',
   });
 
-  console.log('First:');
-  get(`https://crossorigin.me/${url}`)
-    .then(response => console.log(response))
-    .catch(error => console.log(error));
-
-  console.log('Second:');
   instance.get(url)
-    .then(response => console.log(response))
-    .catch(error => console.log(error));
+    .then((response) => {
+      console.log('Second:');
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log('Second:');
+      console.log(error);
+    });
 };
 
 export default parseRSS;
